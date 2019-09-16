98885bee857106cfa25745043d0441e2d6fd4b5e
Patrick Neff <pbneff@yahoo.com>
2019-09-15 18:35:35 -0600

    migrated

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 # Create your models here.
 
-class Article(models.Model)
+class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     # add in thumbnail later
     # add in author later
+
+
+
+# everytime a model is made or changed this connects the model to the database
+    # python manage.py makemigrations
+    # python manage.py migrate
