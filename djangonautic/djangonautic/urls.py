8afdbb7267ea5825184bbb4a386c5a985ed8f811
Patrick Neff <pbneff@yahoo.com>
2019-09-29 11:29:14 -0600

    css linked up

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -1,7 +1,8 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
-from .import views
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,3 +11,6 @@ urlpatterns = [
     url(r'^$', views.homepage),
 
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
