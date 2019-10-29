35534b61bf77a45c8caf3ae202ec52fc355c60c1
Patrick Neff <pbneff@yahoo.com>
2019-10-29 11:47:33 -0600

    signup form and vaildation

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,10 +1,14 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def signup_view(request):
-    form = UserCreationForm()
-    return render(request, 'accounts/signup.html',{ 'form': form })
+    if request.method == 'POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+             form.save()
+             #  log the user in
+             return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', { 'form': form })
 
