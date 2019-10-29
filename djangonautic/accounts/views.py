f5cf2daa95974bdab9833bb091cccf200f8bfd8d
Patrick Neff <pbneff@yahoo.com>
2019-10-29 11:04:13 -0600

    form with auth

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    form = UserCreationForm()
+    return render(request, 'accounts/signup.html',{ 'form': form })
 
